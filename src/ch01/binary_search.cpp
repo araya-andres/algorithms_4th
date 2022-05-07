d28e43248f52f726091e782d1c9264911c68088a
Andres Araya <araya.andres@gmail.com>
2022-05-07 17:30:18 -0600

    initial commit

---
+++ src/ch01/binary_search.cpp
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+std::vector<int> read_ints(std::istream&& is)
+{
+    std::vector<int> ints;
+    int val;
+    while (is >> val) ints.push_back(val);
+    return ints;
+}
+
+int rank(const int key, const std::vector<int>& a)
+{
+    // Array must be sorted.
+    size_t lo = 0;
+    size_t hi = a.size() - 1;
+    while (lo <= hi) {
+        // Key is in a[lo..hi] or not present.
+        size_t mid = lo + (hi - lo) / 2;
+        if (key < a[mid]) hi = mid - 1;
+        else if (key > a[mid]) lo = mid + 1;
+        else return a[mid];
+    }
+    return -1;
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc == 1) return EXIT_FAILURE;
+    auto whitelist = read_ints(std::fstream{argv[1]});
+    std::sort(whitelist.begin(), whitelist.end());
+    // Read key, print if not in whitelist.
+    int key;
+    while (std::cin >> key) {
+        if (rank(key, whitelist) == -1) {
+            std::cout << key << '\n';
+        }
+    }
+}
