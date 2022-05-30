9b8d3c9d423794089adb8454f7806a5163928ab6
Andres Araya <araya.andres@gmail.com>
2022-05-29 18:39:51 -0600

    test code for cc

--- src/ch04/test-cc.cpp
+++ src/ch04/test-cc.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <iostream>
+#include <queue>
 #include "ch04/cc.hpp"
 
 int main(int argc, const char* argv[])
@@ -8,5 +9,16 @@ int main(int argc, const char* argv[])
     auto g = Graph::read_from_stream(std::fstream{argv[1]});
     auto cc = CC{g};
 
-    std::cout << cc.count() << " components\n";
+    auto m = cc.count();
+    std::cout << m << " components\n";
+    std::vector<std::vector<Vertice>> components(m);
+    for (auto v = 0ul; v < g.V(); v++) {
+        components[cc.id(v)].push_back(v);
+    }
+    for (const auto& c: components) {
+        for (auto v: c) {
+            std::cout << v << " ";
+        }
+        std::cout << '\n';
+    }
 }
