c60b658a7df5de37962bc792c82a5d3b41509bda
Andres Araya <araya.andres@gmail.com>
2022-06-11 13:52:19 -0600

    cycle / two-color

---
+++ src/ch04/test-cycle.cpp
@@ -0,0 +1,16 @@
+#include <fstream>
+#include <iostream>
+#include "ch04/cycle.hpp"
+#include "ch04/two-color.hpp"
+
+int main(int argc, const char* argv[])
+{
+    if (argc < 2) return EXIT_FAILURE;
+    auto g = Graph::read_from_stream(std::fstream{argv[1]});
+
+    auto cycle = Cycle{g};
+    std::cout << cycle.has_cycle() << '\n';
+
+    auto two_color = TwoColor{g};
+    std::cout << two_color.is_bipartite() << '\n';
+}
