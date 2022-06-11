2e78758bf95a3487cd8cc5649fb3686e0e8d5043
Andres Araya <araya.andres@gmail.com>
2022-06-11 17:19:40 -0600

    fix interface

--- include/ch04/symbol-graph.hpp
+++ include/ch04/symbol-graph.hpp
@@ -12,6 +12,7 @@
 struct SymbolGraph
 {
     using Edge = std::tuple<std::string, std::string>;
+    using Keys = std:vector<std::string>;
 
     SymbolGraph(std::istream&& is, std::string_view delimiter)
     {
@@ -25,7 +26,7 @@ struct SymbolGraph
             if (!st_.contains(v)) st_[v] = st_.size();
             edges.push_back({u, v});
         }
-        keys_ = std::vector<std::string>(st_.size());
+        keys_ = Keys(st_.size());
         for (auto& [key, index]: st_) {
             keys_[index] = key;
         }
@@ -36,20 +37,20 @@ struct SymbolGraph
     }
 
     // is key a vertex?
-    bool contains(const std::string& key) const { return st_.contains(key); }
+    bool contains(const std::string& key) const noexcept { return st_.contains(key); }
 
     // index associated with key
     Vertice index(const std::string& key) const { return st_.at(key); };
 
     // key associated with index
-    const std::string& name(Vertice v) const { return keys_[v]; };
+    const std::string& name(Vertice v) const { return keys_.at(v); };
 
     // underlying graph
-    const Graph& g() const { return g_; }
+    const Graph& g() const noexcept { return g_; }
 
 private:
 
     std::map<std::string, Vertice> st_;
-    std::vector<std::string> keys_;
+    Keys keys_;
     Graph g_{0};
 };
