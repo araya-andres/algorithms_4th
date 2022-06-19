932e6ff28107e97a33605f021ce0c64a99c56f41
Andres Araya <araya.andres@gmail.com>
2022-06-19 11:08:29 -0600

    find a directed cycle

---
+++ include/ch05/directed-cycle.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "ch05/digraph.hpp"
+
+struct DirectedCycle
+{
+    DirectedCycle(const Digraph& g)
+        : on_stack_(g.V())
+        , edge_to_(g.V())
+        , marked_(g.V())
+    {
+        for (auto v = 0ul; v < g.V(); v++) {
+            if (!marked_[v]) dfs(g, v);
+        }
+    }
+
+    bool has_cycle() const { return !cycle_.empty(); }
+
+    const Edges& cycle() const { return cycle_; }
+
+private:
+
+    void dfs(const Digraph& g, Vertice v)
+    {
+        on_stack_[v] = true;
+        marked_[v] = true;
+        for (const auto w: g.adj(v)) {
+            if (has_cycle()) {
+                return;
+            } else if (!marked_[w]) {
+                edge_to_[w] = v;
+                dfs(g, w);
+            } else if (on_stack_[w]) {
+                for (auto x = v; x != w; x = edge_to_[x]) {
+                    cycle_.push_front(x);
+                }
+                cycle_.push_front(w);
+                cycle_.push_front(v);
+            }
+        }
+        on_stack_[v] = false;
+    }
+
+    std::vector<bool> on_stack_;
+    std::vector<Vertice> edge_to_;
+    std::vector<bool> marked_;
+    Edges cycle_;
+};
--- src/ch05/test-digraph.cpp
+++ src/ch05/test-digraph.cpp
@@ -1,6 +1,9 @@
 #include <fstream>
+#include <iomanip>
 #include <iostream>
+
 #include "ch05/digraph.hpp"
+#include "ch05/directed-cycle.hpp"
 #include "ch05/directed-dfs.hpp"
 
 int main(int argc, char* argv[])
@@ -10,16 +13,27 @@ int main(int argc, char* argv[])
     auto r = g.reverse();
     std::cout << g.str() << '\n'
               << r.str() << '\n';
-    if (argc == 2) return EXIT_SUCCESS;
-    std::vector<size_t> sources;
-    sources.reserve(static_cast<size_t>(argc - 2));
-    std::transform(
-        argv + 2, argv + argc,
-        std::back_inserter(sources),
-        [](const char* arg){ return std::stoul(arg); });
-    auto reachable = DirectedDFS(g, sources);
-    for (auto v = 0ul; v < g.V(); v++) {
-        if (reachable.marked(v)) std::cout << v << ' ';
+    if (argc > 2)
+    {
+        std::vector<size_t> sources;
+        sources.reserve(static_cast<size_t>(argc - 2));
+        std::transform(
+            argv + 2, argv + argc,
+            std::back_inserter(sources),
+            [](const char *arg)
+            { return std::stoul(arg); });
+        auto reachable = DirectedDFS(g, sources);
+        for (auto v = 0ul; v < g.V(); v++)
+        {
+            if (reachable.marked(v))
+                std::cout << v << ' ';
+        }
+        std::cout << '\n';
     }
+    DirectedCycle dc{g};
+    std::cout << std::boolalpha << dc.has_cycle() << '\n';
+    std::copy(
+        dc.cycle().cbegin(), dc.cycle().cend(),
+        std::ostream_iterator<Vertice>(std::cout, " "));
     std::cout << '\n';
 }
