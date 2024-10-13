2714d6a5a63a458f5d368d1514db0403a3e75b83
Andres Araya <araya.andres@gmail.com>
2024-10-13 09:06:06 -0600

    add asserts

--- tests/binary_search.cpp
+++ tests/binary_search.cpp
@@ -8,16 +8,18 @@ TEST(rank, ReturnsNotFoundIfTheVectorIsEmpty)
 
 TEST(rank, WorksProperlyWithASingleValueArray)
 {
-    EXPECT_EQ(binary_search::rank(0, {0}), 0);
     EXPECT_EQ(binary_search::rank(-1, {0}), -1);
+    EXPECT_EQ(binary_search::rank( 0, {0}),  0);
+    EXPECT_EQ(binary_search::rank( 1, {0}), -1);
 }
 
-TEST(rank, WorksProperlyWithATwoValuesArray)
+TEST(rank, WorksProperlyWithAThreeValuesArray)
 {
-    const auto a = std::vector{0, 1};
-    EXPECT_EQ(binary_search::rank(0, a), 0);
-    EXPECT_EQ(binary_search::rank(1, a), 1);
+    const auto a = std::vector{0, 1, 2};
     EXPECT_EQ(binary_search::rank(-1, a), -1);
+    EXPECT_EQ(binary_search::rank( 0, a),  0);
+    EXPECT_EQ(binary_search::rank( 2, a),  2);
+    EXPECT_EQ(binary_search::rank( 3, a), -1);
 }
 
 TEST(rank_r, ReturnsNotFoundIfTheVectorIsEmpty)
@@ -27,14 +29,16 @@ TEST(rank_r, ReturnsNotFoundIfTheVectorIsEmpty)
 
 TEST(rank_r, WorksProperlyWithASingleValueArray)
 {
-    EXPECT_EQ(binary_search::rank_r(0, {0}), 0);
     EXPECT_EQ(binary_search::rank_r(-1, {0}), -1);
+    EXPECT_EQ(binary_search::rank_r( 0, {0}),  0);
+    EXPECT_EQ(binary_search::rank_r( 1, {0}), -1);
 }
 
-TEST(rank_r, WorksProperlyWithATwoValuesArray)
+TEST(rank_r, WorksProperlyWithAThreeValuesArray)
 {
-    const auto a = std::vector{0, 1};
-    EXPECT_EQ(binary_search::rank_r(0, a), 0);
-    EXPECT_EQ(binary_search::rank_r(1, a), 1);
+    const auto a = std::vector{0, 1, 2};
     EXPECT_EQ(binary_search::rank_r(-1, a), -1);
+    EXPECT_EQ(binary_search::rank_r( 0, a),  0);
+    EXPECT_EQ(binary_search::rank_r( 2, a),  2);
+    EXPECT_EQ(binary_search::rank_r( 3, a), -1);
 }
